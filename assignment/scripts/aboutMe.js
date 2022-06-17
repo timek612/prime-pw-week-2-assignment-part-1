481b17602e43b99f4f549eb31f627ac3854007f9
AJ Timek <Timek612@gmail.com>
2022-06-17 17:27:34 -0500

    Completed stretch goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -65,6 +65,27 @@ else{
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+pets = 1;
+friendsPets = 1;
+let mostPets = 0;
+
+if(pets > friendsPets){
+    mostPets = pets;
+    console.log('I have the most pets!');
+    console.log(mostPets);
+}
+else if(friendsPets > pets){
+    mostPets = friendsPets;
+    console.log('My friend has the most pets!');
+    console.log(mostPets);
+}
+else{
+    console.log('We have the same amount of pets!');
+    mostPets = pets;
+    console.log(mostPets);
+}
+
+
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -72,7 +93,29 @@ else{
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+luckyNumber = 26
+
+switch(luckyNumber){
+    case "1":
+        console.log('First is the worst');
+        break;
+    case "2":
+        console.log('Second is the best');
+        break;
+    case "3":
+        console.log('Third is the one with the polka dot dress');
+        break;
+    default:
+        console.log('Luck is what happens when preparation meets opportunity');
+      break;
+}
+//More efficient than an if else statement if you have a lot of possibilities with no nested statements (I think)
+
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+adventurous = true;
+let decision = (adventurous) ? 'Lets go on and adventure!' : 'How about we stay home?';
+console.log(decision);
+
 
 
